93b85a5f662efa369a41da14c64ad42e61a2d835
Suriya Narayanan J <suriyanarayanan176@gmail.com>
2026-02-17 20:37:44 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,9 @@
+#name=input("what is your name ?\n")
+#rint("your  name is  " + name)
+print("welcome to bmsit:")
+id_card=str(input("enter you have id or not?yes or no"))
+
+if id_card==yes:
+       print("you are allowed")
+else:
+    print("your not allowed ")
